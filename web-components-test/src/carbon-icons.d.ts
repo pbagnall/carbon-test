f29bfc7786765ff7dbbb3651e85ce0556a4cc537
Peter Bagnall <peter.bagnall@ul.com>
2025-11-18 14:37:30 +0000

    more declarative icons

---
+++ web-components-test/src/carbon-icons.d.ts
@@ -0,0 +1,5 @@
+// Type declarations for Carbon icons
+declare module '@carbon/icons/es/*' {
+  const icon: any;
+  export default icon;
+}
--- web-components-test/src/index.ts
+++ web-components-test/src/index.ts
@@ -1,27 +1,88 @@
 import "./style.scss";
 import '@carbon/web-components/es/components/button/button.js';
 import "@carbon/web-components/es/components/ui-shell/index";
+
+// Import and register the original icon component first
 import "@carbon/web-components/es/components/icon/index.js";
 
-// Import and register icons globally
+// Import icons
 import Search16 from "@carbon/icons/es/search/16.js";
 import Notification16 from "@carbon/icons/es/notification/16.js";
 import Switcher16 from "@carbon/icons/es/switcher/16.js";
 
-// Make icons available globally for declarative use
-(window as any).carbonIcons = {
+// Create icon registry
+const iconRegistry: Record<string, any> = {
   search: Search16,
   notification: Notification16,
   switcher: Switcher16,
 };
 
-// Set up icons declaratively after DOM loads
-document.addEventListener('DOMContentLoaded', () => {
-  const icons = document.querySelectorAll('cds-icon[icon]');
-  icons.forEach((iconElement: Element) => {
-    const iconName = iconElement.getAttribute('icon');
-    if (iconName && (window as any).carbonIcons[iconName]) {
-      (iconElement as any).icon = (window as any).carbonIcons[iconName];
+// Function to process an icon element
+function processIconElement(iconElement: HTMLElement) {
+  const iconAttr = iconElement.getAttribute('icon');
+  if (iconAttr && typeof iconAttr === 'string') {
+    const iconData = iconRegistry[iconAttr];
+    if (iconData) {
+      (iconElement as any).icon = iconData;
+    } else {
+      console.warn(`Icon "${iconAttr}" not found in registry. Available icons:`, Object.keys(iconRegistry));
     }
+  }
+}
+
+// Wait for the cds-icon component to be defined, then enhance all instances
+customElements.whenDefined('cds-icon').then(() => {
+  
+  // Create a custom element that extends HTMLElement to intercept cds-icon creation
+  class IconRegistry extends HTMLElement {
+    static observedAttributes = ['icon'];
+    
+    connectedCallback() {
+      if (this.tagName.toLowerCase() === 'cds-icon') {
+        processIconElement(this);
+      }
+    }
+    
+    attributeChangedCallback(name: string, oldValue: string | null, newValue: string | null) {
+      if (name === 'icon' && newValue !== oldValue && this.tagName.toLowerCase() === 'cds-icon') {
+        processIconElement(this);
+      }
+    }
+  }
+  
+  // Monkey patch the original cds-icon to add our functionality
+  const originalCDSIcon = customElements.get('cds-icon');
+  if (originalCDSIcon) {
+    const originalConnectedCallback = originalCDSIcon.prototype.connectedCallback;
+    const originalAttributeChangedCallback = originalCDSIcon.prototype.attributeChangedCallback;
+    
+    // Enhance connectedCallback
+    originalCDSIcon.prototype.connectedCallback = function() {
+      originalConnectedCallback?.call(this);
+      processIconElement(this);
+    };
+    
+    // Enhance attributeChangedCallback
+    originalCDSIcon.prototype.attributeChangedCallback = function(name: string, oldValue: string | null, newValue: string | null) {
+      originalAttributeChangedCallback?.call(this, name, oldValue, newValue);
+      if (name === 'icon' && newValue !== oldValue) {
+        processIconElement(this);
+      }
+    };
+    
+    // Ensure 'icon' attribute is observed
+    const originalObservedAttributes = (originalCDSIcon as any).observedAttributes || [];
+    if (!originalObservedAttributes.includes('icon')) {
+      Object.defineProperty(originalCDSIcon, 'observedAttributes', {
+        get() {
+          return [...originalObservedAttributes, 'icon'];
+        }
+      });
+    }
+  }
+  
+  // Process any existing cds-icon elements
+  document.querySelectorAll('cds-icon[icon]').forEach((element) => {
+    processIconElement(element as HTMLElement);
   });
 });
