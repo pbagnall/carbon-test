5036e06bbed976801bc8b10aca4de77a672abc4b
Peter Bagnall <peter.bagnall@ul.com>
2025-11-19 16:59:30 +0000

    Added an Angular app

--- angular-test/src/app/app.ts
+++ angular-test/src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('angular-test');
+  hasHamburger = false;
+
+  toggleHamburger() {
+    this.hasHamburger = !this.hasHamburger;
+  }
+
+  buttonClick() {
+    alert('Clicked');
+  }
 }
--- angular-test/src/main.ts
+++ angular-test/src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+// Import Carbon Web Components
+import '@carbon/web-components/es/components/ui-shell/index.js';
+import '@carbon/web-components/es/components/button/index.js';
+import '@carbon/web-components/es/components/icon/index.js';
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
